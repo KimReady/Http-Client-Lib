57893701ab58d6a6d5b42b36e4e6f8441a3b9e17
JunBi Kim <junbi.kim@navercorp.com>
2019-07-12 15:19:42 +0900

    Annotation 정의

---
+++ HttpClientLib/src/main/java/com/naver/httpclientlib/annotation/FieldMap.java
@@ -0,0 +1,13 @@
+package com.naver.httpclientlib.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface FieldMap {
+    boolean encoded() default false;
+}
---
+++ HttpClientLib/src/main/java/com/naver/httpclientlib/annotation/RequestMapping.java
@@ -0,0 +1,17 @@
+package com.naver.httpclientlib.annotation;
+
+import com.naver.httpclientlib.RequestMethod;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface RequestMapping {
+
+    String value() default "";
+    RequestMethod method() default RequestMethod.GET;
+}
