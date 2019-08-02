ed19ce57a19ea242439e39c59aaf2a77da240a37
JunBi Kim <junbi.kim@navercorp.com>
2019-08-02 11:12:13 +0900

    implements configuration fragment for HttpClient and Timeout

--- HttpClientLib-demo/src/main/java/com/naver/httpclienttest/HttpClientConfigFragment.java
+++ HttpClientLib-demo/src/main/java/com/naver/httpclienttest/HttpClientConfigFragment.java
@@ -1,13 +1,13 @@
 package com.naver.httpclienttest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,10 +24,16 @@ public class HttpClientConfigFragment extends Fragment {
     EditText connectTimeoutText;
     EditText readTimeoutText;
     EditText writeTimeoutText;
+    SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sharedPreferences = getContext().getSharedPreferences(
+                getContext().getResources().getString(R.string.config_file),
+                Context.MODE_PRIVATE
+        );
     }
 
     @Nullable
@@ -38,24 +44,76 @@ public class HttpClientConfigFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_httpclient_config, container, false);
 
         baseUrlText = view.findViewById(R.id.base_url);
+        baseUrlText.setText(sharedPreferences.getString(
+                "baseUrl",
+                getContext().getResources().getString(R.string.default_URL)));
+
         callTimeoutText = view.findViewById(R.id.call_time);
+        callTimeoutText.setText(sharedPreferences.getString(
+                "callTimeout",
+                String.valueOf(10000)
+        ));
         connectTimeoutText = view.findViewById(R.id.connect_time);
+        connectTimeoutText.setText(sharedPreferences.getString(
+                "connectTimeout",
+                String.valueOf(10000)
+                ));
         readTimeoutText = view.findViewById(R.id.read_time);
+        readTimeoutText.setText(sharedPreferences.getString(
+                "readTimeout",
+                String.valueOf(10000)
+        ));
         writeTimeoutText = view.findViewById(R.id.write_time);
+        writeTimeoutText.setText(sharedPreferences.getString(
+                "writeTimeout",
+                String.valueOf(10000)
+        ));
 
         Button saveBtn = view.findViewById(R.id.save_btn);
         saveBtn.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
 
-                MainFragment mainFragment = new MainFragment();
-                fragmentTransaction.replace(R.id.fragment_container, mainFragment);
-                fragmentTransaction.commit();
+                CharSequence baseUrlSeq = baseUrlText.getText();
+                if(baseUrlSeq != null) {
+                    editor.putString("baseUrl", baseUrlSeq.toString());
+                }
+
+                CharSequence callSeq = callTimeoutText.getText();
+                if(callSeq.length() > 0) {
+                    editor.putInt("callTimeout", Integer.valueOf(callSeq.toString()));
+                }
+
+                CharSequence connectSeq = connectTimeoutText.getText();
+                if(connectSeq.length() > 0) {
+                    editor.putInt("connectTimeout", Integer.valueOf(connectSeq.toString()));                }
+
+                CharSequence readSeq = readTimeoutText.getText();
+                if(readSeq.length() > 0) {
+                    editor.putInt("readTimeout", Integer.valueOf(readSeq.toString()));
+                }
+
+                CharSequence writeSeq = writeTimeoutText.getText();
+                if(writeSeq.length() > 0) {
+                    editor.putInt("writeTimeout", Integer.valueOf(writeSeq.toString()));
+                }
+
+                editor.apply();
+
+                replaceFragment(new MainFragment());
             }
         });
 
         return view;
     }
+
+    private void replaceFragment(Fragment newFragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, newFragment);
+        fragmentTransaction.commit();
+    }
+
 }
